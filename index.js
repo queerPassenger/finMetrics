c52006767eae261671520290481d8e93ab366b53
allanvalooran <allanmvalooran@gmail.com>
2018-09-16 14:12:12 +0530

    RepositoryNameChange

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs=require('fs');
 let companyList=['altimetrik'];
 let selectedCompany='altimetrik';
-let finAnalyser= require('./organization/'+selectedCompany+'.js');
+let output= require('./organization/'+selectedCompany+'.js');
 
-finAnalyser.fileReader((_object)=>{
+output.fileReader((_object)=>{
     fs.writeFile("./des/pdfRawData.json",JSON.stringify(_object),(err)=>{
         if(!err){
             console.log('DONE');
